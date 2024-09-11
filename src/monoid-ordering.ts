963f8e781ab302a4da4faf0237ce1e9ad547b221
Phil Beadling <phil.beadling@opetek.io>
2024-09-11 21:11:44 +0100

    tidy

--- src/monoid-ordering.ts
+++ src/monoid-ordering.ts
@@ -2,7 +2,6 @@
 
 import { combineAll, Ordering } from "effect/Ordering"; // Importing combineAll for combining orderings
 import * as Ord from "effect/Order"; // Importing all Order instances and utilities from Order module
-import * as O from "@effect/typeclass/data/Ordering"; // Importing Ordering related Semigroup and Monoid
 
 // lengthCompare :: String -> String -> Ordering
 // lengthCompare x y = (length x `compare` length y) `mappend`
@@ -15,20 +14,18 @@ const countVowels = (s: string): number =>
   s.split('').filter(c => 'aeiou'.indexOf(c.toLowerCase()) !== -1).length;
 
 // Order instance for comparing strings by their length
+// mapInput is similar to, but a more narrow Order-specific use-case of contramap in fp-ts
 const ordLength = Ord.mapInput((s: string) => s.length)(Ord.number);
 
 // Order instance for comparing strings by their number of vowels
 const ordVowels = Ord.mapInput((s: string) => countVowels(s))(Ord.number);
-
-// Order instance for lexicographic comparison of strings
-const ordLexical = Ord.string;
 
 // Combined comparison function using combineAll
 const lengthCompare = (x: string, y: string): Ordering => 
   combineAll([
     ordLength(x, y),  // Compare by length
     ordVowels(x, y),  // Compare by number of vowels
-    ordLexical(x, y)  // Compare lexicographically
+    Ord.string(x, y)  // Compare standard - lexicographically
   ]);
 
 // Testing the lengthCompare function
