dea2a5e15ae3c8c2eaebd8d5609f4ea95999d417
Phil Beadling <phil.beadling@opetek.io>
2024-09-13 13:04:06 +0100

    factor out Option's SemiApplicative

--- src/applicative.ts
+++ src/applicative.ts
@@ -13,20 +13,23 @@ const add3 = (x: number) => (y: number) => (z: number): number => x + y + z;
 const addWrapped = Option.some(add);
 const addWrapped3 = Option.some(add3);
 
+// Create a version of ap specifically for our Applicative instance
+const ap = SA.ap(O.SemiApplicative);
+
 // Using SemiApplicative's `ap` method to apply the curried functions
 const result = pipe(
   addWrapped,                                    // Start with the wrapped function
-  SA.ap(O.SemiApplicative)(Option.some(3)),  // Apply to the first Option (x = 3)
-  SA.ap(O.SemiApplicative)(Option.some(5))   // Apply to the second Option (y = 5)
+  ap(Option.some(3)),  // Apply to the first Option (x = 3)
+  ap(Option.some(5))   // Apply to the second Option (y = 5)
 );
 
 console.log(result); // Output: some(8)
 
 const result2 = pipe(
   addWrapped3,                                   // Start with the wrapped function
-  SA.ap(O.SemiApplicative)(Option.some(10)), // Apply to the first Option (x = 10)
-  SA.ap(O.SemiApplicative)(Option.some(6)),  // Apply to the second Option (y = 6)
-  SA.ap(O.SemiApplicative)(Option.some(3))   // Apply to the third Option (z = 3)
+  ap(Option.some(10)), // Apply to the first Option (x = 10)
+  ap(Option.some(6)),  // Apply to the second Option (y = 6)
+  ap(Option.some(3))   // Apply to the third Option (z = 3)
 );
 
 console.log(result2); // Output: some(19)
