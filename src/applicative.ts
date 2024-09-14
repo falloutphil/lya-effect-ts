662f46ddda8b2ac0d173ef3c8c9fa0b4c5b7490d
Phil Beadling <phil.beadling@opetek.io>
2024-09-14 10:40:17 +0100

    make code more generic

--- src/applicative.ts
+++ src/applicative.ts
@@ -1,7 +1,6 @@
 // -*- compile-command: "npx ts-node applicative.ts" -*-
 
 import { pipe } from "effect/Function";
-import * as Option from "effect/Option";
 import * as O from "@effect/typeclass/data/Option";
 import * as SA from "@effect/typeclass/SemiApplicative";
 
@@ -9,27 +8,29 @@ import * as SA from "@effect/typeclass/SemiApplicative";
 const add = (x: number) => (y: number): number => x + y;
 const add3 = (x: number) => (y: number) => (z: number): number => x + y + z;
 
-// Wrapped functions in Option functors
-const addWrapped = Option.some(add);
-const addWrapped3 = Option.some(add3);
+// Create a version of ap and of specifically for our Applicative instance
+const ap = SA.ap(O.Applicative);
+const of = O.Applicative.of;
 
-// Create a version of ap specifically for our Applicative instance
-const ap = SA.ap(O.SemiApplicative);
+// Wrapped functions in Option functors
+const addWrapped = of(add);
+const addWrapped3 = of(add3);
+
 
 // Using SemiApplicative's `ap` method to apply the curried functions
 const result = pipe(
   addWrapped,                                    // Start with the wrapped function
-  ap(Option.some(3)),  // Apply to the first Option (x = 3)
-  ap(Option.some(5))   // Apply to the second Option (y = 5)
+  ap(of(3)),  // Apply to the first Option (x = 3)
+  ap(of(5))   // Apply to the second Option (y = 5)
 );
 
 console.log(result); // Output: some(8)
 
 const result2 = pipe(
   addWrapped3,                                   // Start with the wrapped function
-  ap(Option.some(10)), // Apply to the first Option (x = 10)
-  ap(Option.some(6)),  // Apply to the second Option (y = 6)
-  ap(Option.some(3))   // Apply to the third Option (z = 3)
+  ap(of(10)), // Apply to the first Option (x = 10)
+  ap(of(6)),  // Apply to the second Option (y = 6)
+  ap(of(3))   // Apply to the third Option (z = 3)
 );
 
 console.log(result2); // Output: some(19)
